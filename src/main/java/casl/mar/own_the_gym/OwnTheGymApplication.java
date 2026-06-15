5700915785bd59f24fe174e0d543d3b52ac2b7c6
IGORMASL <workigormas.20@gmail.com>
2026-06-15 23:32:46 +0700

    Initial Commit

---
+++ src/main/java/casl/mar/own_the_gym/OwnTheGymApplication.java
@@ -0,0 +1,13 @@
+package casl.mar.own_the_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnTheGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnTheGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/casl/mar/own_the_gym/OwnTheGymApplicationTests.java
@@ -0,0 +1,13 @@
+package casl.mar.own_the_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnTheGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
